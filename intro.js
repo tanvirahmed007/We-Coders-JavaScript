10db7f0dd6fdbb14214d4f29f0a3ab824829b379
Tanvir Ahmed <tanvir15-9909@diu.edu.bd>
2022-09-15 22:54:42 +0600

    variable declear, value assign, data types, sum, sub, mul, div

---
+++ intro.js
@@ -0,0 +1,49 @@
+console.log("We Coders presents: Intro to JavaScript");
+
+// variables
+var a;
+var b;
+a=10;
+b=20;
+
+var c = a+b;
+console.log(c);
+
+var x = 50;
+var y = 100;
+var z = x+y;
+console.log("Sum of x and y : ",z);
+
+
+var name = "We Coders";
+console.log("The String is: ",name);
+
+var name2;
+name2 = "JavaScript";
+console.log("The String is: ",name2);
+
+
+var bool = true;
+console.log("The Boolean is: ",bool);
+
+var bool2 = false;
+console.log("The Boolean is: ",bool2);
+
+
+// sum, sub, mul, div
+
+var num1;
+var num2;
+
+num1 = 40;
+num2 = 10;
+
+var sum = num1+num2;
+var sub = num1-num2;
+var mul = num1*num2;
+var div = num1/num2;
+
+console.log("Sum of num1 and num2 : ",sum);
+console.log("Sub of num1 and num2 : ",sub);
+console.log("Mul of num1 and num2 : ",mul);
+console.log("Div of num1 and num2 : ",div);
